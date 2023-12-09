9547fbafa43fb3e1b8dd6a8f00fc8ef9695d5d96
Asiiaaaa <anastasiya23091988@gmail.com>
2023-12-10 02:00:41 +0300

    add exercise1

--- HomeWork002/Program.cs
+++ HomeWork002/Program.cs
@@ -1,2 +1,20 @@
 ﻿//Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
-Console.WriteLine("Введите число")
+Console.WriteLine("Введите число");
+int n1 = Convert.ToInt32 (Console.ReadLine());
+int n2 = 161;
+
+
+if (n1%n2 == 0)
+{
+Console.WriteLine("yes");
+}
+
+else 
+
+{
+Console.WriteLine("no");
+}
+
+    
+
+    
